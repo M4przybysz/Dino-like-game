85a7e92038f8382ef8c1b0a5d3072e7f3f02ceae
Maciej <maprz2017@gmail.com>
2021-10-07 22:43:22 +0200

    Update main.cpp

--- game-SFML/source code/main.cpp
+++ game-SFML/source code/main.cpp
@@ -1,6 +1,6 @@
-#include "SFML-2.5.1\include\SFML/Graphics.hpp"
-#include "SFML-2.5.1\include\SFML/Window.hpp"
-#include "SFML-2.5.1\include\SFML/System.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
 
 using namespace std;
 using namespace sf;
